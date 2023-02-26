2e5c9c54fe999036821ddb40458c1eef809645ed
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 11:18:55 -0500

    write create controller and add re.body.profileId...

--- controllers/meals.js
+++ controllers/meals.js
@@ -1,6 +1,16 @@
 const { Meal } = require('../models')
 
+const create = async (req, res) => {
+  try {
+    req.body.profileId = req.user.profile.id
+    const meal = await Meal.create(req.body)
+    res.status(200).json(meal)
+  } catch (error) {
+
+  }
+}
+
 
 module.exports = {
-
+  create,
 }
