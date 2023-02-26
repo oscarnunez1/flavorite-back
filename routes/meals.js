2eb11e2703db7a638f5f53abe5906f96665217a4
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 11:20:53 -0500

    define index route in routes

--- routes/meals.js
+++ routes/meals.js
@@ -5,11 +5,12 @@ const middleware = require('../middleware/auth.js')
 const { decodeUserFromToken, checkAuth } = middleware
 
 /*---------- Public Routes ----------*/
-
+router.get('/', mealsCtrl.index)
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
 router.post('/', checkAuth, mealsCtrl.create)
+router.get('/', checkAuth, mealsCtrl.index)
 
 
 module.exports = router
