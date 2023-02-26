d7de10b97c589a0982701d7112f121863a7274e7
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 10:37:30 -0500

    create meal migration and model

---
+++ migrations/20230226153649-create-meal.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Meals', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      photo: {
+        type: Sequelize.STRING
+      },
+      profileId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Meals');
+  }
+};
---
+++ models/meal.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Meal extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Meal.init({
+    name: DataTypes.STRING,
+    description: DataTypes.TEXT,
+    photo: DataTypes.STRING,
+    profileId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Meal',
+  });
+  return Meal;
+};
