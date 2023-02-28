2bbf2bc332e3c505ae47ad4ea14d4855852f89b3
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-28 12:51:21 -0500

    update the create and update controllers to fix front end refresh issue

--- controllers/meals.js
+++ controllers/meals.js
@@ -3,8 +3,18 @@ const { Meal, Profile } = require('../models')
 async function create(req, res) {
   try {
     req.body.profileId = req.user.profile.id
-    const meal = await Meal.create(req.body)
-    res.status(200).json(meal)
+    await Meal.create(req.body)
+    const meals = await Meal.findAll({
+      include: [
+        {
+          model: Profile, as: 'profile',
+        }
+      ],
+      order: [
+        ['id', 'DESC']
+      ]
+    })
+    res.status(200).json(meals)
   } catch (error) {
     console.log(error)
     res.status(500).json({ err: error })
@@ -16,7 +26,7 @@ async function index(req, res) {
     const meals = await Meal.findAll({
       include: [
         {
-          model: Profile, as: 'profile' 
+          model: Profile, as: 'profile',
         }
       ],
       order: [
@@ -32,10 +42,15 @@ async function index(req, res) {
 
 async function update(req, res) {
   try {
-    const meal = await Meal.findByPk(req.params.id)
+    const meal = await Meal.findByPk(req.params.id, {
+      include: [
+        {
+          model: Profile, as: 'profile',
+        }
+      ],
+    })
     meal.set(req.body)
     await meal.save()
-
     res.status(200).json(meal)
   } catch (error) {
     res.status(500).json(error)
